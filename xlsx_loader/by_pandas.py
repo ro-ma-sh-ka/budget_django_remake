eebbff1b2c5a837baafe9fc8f6d475337738d7aa
romashka <roma.l@mail.ru>
2023-01-10 19:48:58 +0400

    serialized xlsx

--- xlsx_loader/by_pandas.py
+++ xlsx_loader/by_pandas.py
@@ -1,20 +1,27 @@
 import os
 import pandas as pd
 
-dfs = pd.read_excel('budgetLesh.xlsx', sheet_name=None, usecols='A:E')
-print(dfs.keys())
-# print(dfs['2020'])
-result = pd.concat(dfs)
-print(result)
-
-# workbook = pd.read_excel(excel_file, sheet_name=pattern_sheet, skiprows=[0, 2])
-# combiner = pd.DataFrame()
-# workbook.at[0:workbook.shape[0], 'Шаблон озон'] = file
-# print(f'{counter}/{len(files)} - {file}')
-# counter += 1
-# # concatenate dataframes
-# combiner = pd.concat([combiner, workbook], sort=False, axis=0, ignore_index=True)
-# result_file = os.path.join(work_dir, '_combiner_result.xlsx')
-# write_to_excel(result_file, pattern_sheet, combiner)
+# dfs = pd.read_excel('budgetLesh.xlsx', sheet_name=None, usecols='A:E')
+# # print(dfs.keys())
+# # print(dfs['2020']) - list of sheets
+# result = pd.concat(dfs)
 #
-# combiner.to_excel(writer, sheet_name=pattern_sheet)
+#
+# with pd.ExcelWriter('result_file.xlsx') as writer:
+#     result.to_excel(writer)
+
+dfs2 = pd.read_excel('budgetLesh.xlsx')
+columns = ['year', 'month', 'day']
+dfs2['date'] = dfs2[columns].astype(str).agg('-'.join, axis=1)
+dfs2['date'] = pd.to_datetime(dfs2['date'])
+dfs2 = dfs2.set_index('date')
+dfs2 = dfs2.drop(columns=columns, axis=1)
+
+with pd.ExcelWriter('result_file.xlsx') as writer:
+    dfs2.to_excel(writer)
+
+
+# result.shape[0] - number of rows
+# result.shape[1] - number of columns
+
+

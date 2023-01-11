fb902e4fc27aaa3ab840ef1d866d92ba88fdcc78
romashka <roma.l@mail.ru>
2023-01-11 21:35:35 +0400

    started with connecting dataframe and db

--- xlsx_loader/by_pandas.py
+++ xlsx_loader/by_pandas.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+from sqlalchemy import create_engine
+import psycopg2
 
 # dfs = pd.read_excel('budgetLesh.xlsx', sheet_name=None, usecols='A:E')
 # # print(dfs.keys())
@@ -17,8 +19,19 @@ dfs2['date'] = pd.to_datetime(dfs2['date'])
 dfs2 = dfs2.set_index('date')
 dfs2 = dfs2.drop(columns=columns, axis=1)
 
-with pd.ExcelWriter('result_file.xlsx') as writer:
-    dfs2.to_excel(writer)
+engine = create_engine('postgresql://postgres:1111@localhost:5432/budget_django')
+
+# read id's from section from currency
+sections_id = pd.read_sql_table('budget_section', con=engine)
+currencies_id = pd.read_sql_table('budget_currency', con=engine)
+
+print(currencies_id)
+# engine = create_engine('postgresql://postgres:1111@localhost:5432/budget_django')
+#
+# dfs2.to_sql('budget_budget', con=engine, if_exists='append')
+
+# with pd.ExcelWriter('result_file.xlsx') as writer:
+#     dfs2.to_excel(writer)
 
 
 # result.shape[0] - number of rows
